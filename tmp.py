367b82b1bb3eba77c34f66f6d94787d7115b6bbe
HughLocke <517530707@qq.com>
2020-03-03 20:01:28 +0800

    更新到了16节

--- tmp.py
+++ tmp.py
@@ -4,27 +4,46 @@ import numpy as np
 src = cv.imread("./test.png")
 cv.namedWindow("input", cv.WINDOW_AUTOSIZE)
 cv.imshow("input", src)
+h, w = src.shape[:2]
 
-# X Flip 倒影
-dst1 = cv.flip(src, 0);
-cv.imshow("x-flip", dst1);
+# 获取ROI
+cy = h//2
+cx = w//2
+roi = src[cy-100:cy+100,cx-100:cx+100,:]
+cv.imshow("roi", roi)
 
-# Y Flip 镜像
-dst2 = cv.flip(src, 1);
-cv.imshow("y-flip", dst2);
+# copy ROI
+image = np.copy(roi)
 
-# XY Flip 对角
-dst3 = cv.flip(src, -1);
-cv.imshow("xy-flip", dst3);
+# modify ROI
+roi[:, :, 0] = 0
+cv.imshow("result", src)
 
-# custom y-flip
-h, w, ch = src.shape
-dst = np.zeros(src.shape, src.dtype)
-for row in range(h):
-    for col in range(w):
-        b, g, r = src[row, col]
-        dst[row, w - col - 1] = [b, g, r]
-cv.imshow("custom-y-flip", dst)
+# modify copy roi
+image[:, :, 2] = 0
+cv.imshow("result", src)
+cv.imshow("copy roi", image)
+
+# example with ROI - generate mask
+src2 = cv.imread("./test.png")
+cv.imshow("src2", src2)
+hsv = cv.cvtColor(src2, cv.COLOR_BGR2HSV)
+mask = cv.inRange(hsv, (35, 43, 46), (99, 255, 255))
+
+# extract person ROI
+mask = cv.bitwise_not(mask)
+person = cv.bitwise_and(src2, src2, mask=mask)
+
+# generate background
+result = np.zeros(src2.shape, src2.dtype)
+result[:,:,0] = 255
+
+# combine background + person
+mask = cv.bitwise_not(mask)
+dst = cv.bitwise_or(person, result, mask=mask)
+dst = cv.add(dst, person)
+
+cv.imshow("dst", dst)
 
 cv.waitKey(0)
 cv.destroyAllWindows()
